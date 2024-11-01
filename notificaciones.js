fbf78f9dcf7dd2d9e5f6dffc17ff24992791883d
David-Parada-Amoedo <dparadacursos@gmail.comgit config --global user.email dparadacursos@gmail.com>
2024-11-01 22:04:00 +0100

    reparamos cambios en notificaciones.js

--- notificaciones.js
+++ notificaciones.js
@@ -1,7 +1,6 @@
 /* Creo un archivo notificaciones para hacer 
 el ejercicio 2.3 */
 
-/*Añadimos otra linea para realizar una modificación del
-archivo, epero sea suficiente*/
 
-/*intento provocar un fallo desde Windows*/
+
+
